ea166d0a36621840145590310122366d49f3f332
Manuel Martinez Arizmendi <manmar92@gmail.com>
2016-04-18 19:00:29 -0500

    Logout action implemented

--- app/src/main/java/com/sociit/app/sociit/MainActivity.java
+++ app/src/main/java/com/sociit/app/sociit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sociit.app.sociit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -68,6 +69,12 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_sign_out) {
+            // Redirect to MainActivity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
             return true;
         }
 
