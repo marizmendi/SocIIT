7168a45237a9641649d1dcfba90785418b929362
Lazaro <lsanche7@hawk.iit.edu>
2016-04-21 15:54:05 -0500

    Splash 2.0

---
+++ app/src/main/java/com/sociit/app/sociit/activities/Splash.java
@@ -0,0 +1,46 @@
+package com.sociit.app.sociit.activities;
+
+/**
+ * Created by Lazaro on 4/21/16.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.sociit.app.sociit.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 6000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Show the splash screen
+        setContentView(R.layout.splash);
+
+        //Create a Timer
+        Timer runSplash = new Timer();
+
+        //Task to do when the timer ends
+        TimerTask showSplash = new TimerTask() {
+            @Override
+            public void run() {
+                //Close Splash.class
+                finish();
+
+                //Start QuoteReaderActivity.class
+                Intent mainIntent = new Intent(Splash.this, LoginActivity.class);
+                startActivity(mainIntent);
+            }
+        };
+        //Start the timer
+        runSplash.schedule(showSplash,SPLASH_DISPLAY_LENGTH);
+    }
+}
