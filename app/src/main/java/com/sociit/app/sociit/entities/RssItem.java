bade157e5a3b0a777069f7bde1746be798b427c1
Lazaro <lsanche7@hawk.iit.edu>
2016-04-25 12:43:08 -0500

    RssFeed

---
+++ app/src/main/java/com/sociit/app/sociit/entities/RssItem.java
@@ -0,0 +1,25 @@
+package com.sociit.app.sociit.entities;
+
+/**
+ * Created by Lazaro on 4/25/16.
+ */
+public class RssItem {
+    private String title;
+    private String link;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ app/src/main/java/com/sociit/app/sociit/helpers/RssParseHandler.java
@@ -0,0 +1,70 @@
+package com.sociit.app.sociit.helpers;
+
+import com.sociit.app.sociit.entities.RssItem;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lazaro on 4/25/16.
+ */
+public class RssParseHandler extends DefaultHandler{
+
+    private List <RssItem> rssItems;
+
+    //Used to reference item while parsing
+    private RssItem currentItem;
+
+    //Parsing title indicator
+    private boolean parsingTitle;
+    //Parsing link indicator
+    private boolean parsingLink;
+
+    public RssParseHandler() {
+        rssItems = new ArrayList<RssItem>();
+    }
+
+    public List<RssItem> getRssItems(){
+        return rssItems;
+    }
+
+    @Override
+    public void startElement (String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if ("content-item".equals(qName)){
+            currentItem = new RssItem();
+        } else if ("title".equals(qName)){
+            parsingTitle = true;
+        } else if ("url".equals(qName)){
+            parsingLink = true;
+        }
+    }
+
+    @Override
+    public void endElement (String uri, String localName, String qName) throws SAXException {
+        if ("content-item".equals(qName)){
+            rssItems.add(currentItem);
+            currentItem = null;
+        } else if ("title".equals(qName)){
+            parsingTitle = false;
+        } else if ("url".equals(qName)){
+            parsingLink = false;
+        }
+    }
+
+    @Override
+    public void characters (char[] ch, int start, int length) throws SAXException {
+        if (parsingTitle){
+            if (currentItem != null)
+                currentItem.setTitle(new String(ch, start, length));
+        } else if (parsingLink) {
+            if (currentItem != null) {
+                currentItem.setLink(new String(ch, start, length));
+                parsingLink = false;
+            }
+        }
+    }
+}
